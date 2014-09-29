a330655407277e6f32ba2a54790938d25825fd00
Jordan Chase <jchase@jordan.qpeak.com>
2014-09-29 11:56:24 -0400

    Initial Commit

---
+++ js/theme.js
@@ -0,0 +1,52 @@
+(function($){
+  var width = $(window).width(),
+  breakpoints = {
+    xs: 768,
+    sm: 992,
+    md: 1200
+  },
+  slideNavTimer = setSlideNavTimer();
+  //stuff to do on resize
+  $(window).resize(function(){
+    width = $(window).width();
+  });
+  //stuff to do on scroll
+  $(window).scroll(function(){
+    var scrollY = $(window).scrollTop();
+    /*
+     * show 'get started' button in the header when past the 'get started' 
+     * button in the main carousel
+     */
+     if(width > breakpoints.sm){
+       var gsY = $(".get-started-lg").offset().top;
+       if(scrollY > gsY){
+         $(".get-started-sm").addClass("on");
+       } else {
+         $(".get-started-sm").removeClass("on");
+       }
+     }
+  });
+  //slide nav listeners
+  $(".slide-nav li").on("click", function(){
+    var idx = $(this).index();
+    $(".slide-nav li.active, .slide.active").removeClass("active");
+    $(".slide-nav li:eq("+idx+"), .slide:eq("+idx+")").addClass("active");
+    window.clearInterval(slideNavTimer);
+    //reset the timer after 30 secs
+    setTimeout(function(){
+      slideNavTimer = setSlideNavTimer();
+    },30000)
+  });
+  //slide nav auto scroll
+  function setSlideNavTimer(){
+    var timer = window.setInterval(function(){
+      var next = $(".slide-nav li.active").next().length ? $(".slide-nav li.active").next() : $(".slide-nav li:first");
+      next.click();
+    },5000)
+    return timer;
+  }
+  //scroll arrow listener
+  $(".scroll-arrow").click(function(){
+    $("html,body").animate({scrollTop:$(".main-content").offset().top - $(".page-top").outerHeight()},500);
+  })
+})(jQuery)
